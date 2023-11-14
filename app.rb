1b81d92db68268dff3d62a15003d871e6cbe79af
Cisco Vlahakis <ciscovlahakis@gmail.com>
2023-11-14 06:18:04 +0000

    UI for recurrence mostly complete+page refactoring

--- app.rb
+++ app.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
-require 'sinatra'
-require 'sinatra/reloader'
-require_relative 'config/initialize_firestore'
-require_relative 'models/meal_plan_generator'
+require "sinatra"
+require "sinatra/reloader"
+require_relative "config/initialize_firestore"
+require_relative "models/meal_plan_generator"
 
 helpers do
   def current_user
-    return unless session.fetch('user_id', nil)
+    return unless session.fetch("user_id", nil)
 
-    user_id = session.fetch('user_id', nil)
+    user_id = session.fetch("user_id", nil)
     return unless user_id
 
     matching_users = User.where({ id: user_id })
@@ -17,78 +17,85 @@ helpers do
   end
 end
 
-get('/') do
+def get_data(ref)
+  output = []
+  ref.get do |x|
+    data = x.data
+    string_data = data.transform_keys(&:to_s) # Convert all keys to Strings
+    output << string_data
+  end
+  return output
+end
+
+def navigate_to_page(ref, columns)
+  @data = get_data(ref)
+  @columns = columns
+  erb(:"shared/page")
+end
+
+get("/") do
   erb(:home)
 end
 
-get('/meal-plan') do
+get("/meal-plan") do
   @meal_plan = MealPlanGenerator.new(current_user).generate
   erb(:meal_plan)
 end
 
-get('/nutritional-components') do
-  erb(:nutritional_components)
+get("/nutritional-components") do
+  @title = "Nutritional Components"
+  @description = "Calories, Protein, Sodium..."
+  @create_button_title = "Create Nutritional Component"
+
+  nutritional_components_ref = $firestore.col("nutritionalComponents")
+  nutritional_components_columns = [
+    { id: "name", name: "Name", type: String },
+    { id: "eaten", name: "Eaten", type: Integer },
+    { id: "min", name: "Min", type: Integer },
+    { id: "max", name: "Max", type: Integer },
+    { id: "target", name: "Target", type: Integer },
+    { id: "progress", name: "Progress", type: Integer }
+  ]
+
+  navigate_to_page(nutritional_components_ref, nutritional_components_columns)
 end
 
-get('/foods') do
-  erb(:foods)
+get("/foods") do
+  erb(:"shared/page")
 end
 
-get('/food-categories') do
-  erb(:food_categories)
+get("/food-categories") do
+  erb(:"shared/page")
 end
 
-@meal_events = []
-
-get('/meal-events') do
-  meal_events_ref = $firestore.col('meal_events')
-  @meal_events = []
-
-  meal_events_ref.get do |meal_event|
-    data = meal_event.data
-    string_data = data.transform_keys(&:to_s) # Convert all keys to Strings
-    @meal_events << string_data
-  end
-
-  erb(:meal_events)
+meal_events_ref = $firestore.col("meal_events")
+get("/meal-events") do
+  @title = "Meal Events"
+  @description = "Manage your meal times, types, and constraints."
+  @create_button_title = "Create Meal Event"
+  meal_event_columns = [
+    { id: "name", name: "Name", type: String },
+    { id: "min_foods", name: "Min Foods", type: Integer },
+    { id: "max_foods", name: "Max Foods", type: Integer },
+    { id: "recurrence", name: "Recurrence", type: String },
+  ]
+  navigate_to_page(meal_events_ref, meal_event_columns)
+end
+post("/create-meal-event") do
+  new_meal_event = {
+    "name" => params.fetch("name"),
+    "min_foods" => params.fetch("min-foods", "").to_i,
+    "max_foods" => params.fetch("max-foods", "").to_i,
+    "recurrence" => params.fetch("recurrence", "").to_i,
+  }
+  meal_events_ref.add(new_meal_event)
+  redirect "/meal-events"
 end
 
-# The new post route
-# post('/create-meal-event') do
-#   meal_events_ref = $firestore.col('meal_events')
-
-#   # Here we're creating a new hash with the data from the form
-#   new_meal_event = {
-#     'name' => params.fetch('name'),
-#     # 'parent_meal_event' => params.fetch('parent-meal-event', ''),
-#     # 'min_foods' => params.fetch('min-foods').to_i,
-#     # 'max_foods' => params.fetch('max-foods').to_i,
-#     # 'recurrence' => params.fetch('recurrence').to_i,
-#     # 'recurrence-weekly' => params.fetch('recurrence-weekly').to_i,
-#     # 'days' => params.fetch('days', []),
-#     # 'cuisine' => params.fetch('cuisine')
-#   }
-
-#   # Then we're adding it to Firestore
-#   meal_events_ref.add(new_meal_event)
-
-#   # After the new meal event is created, you might want to redirect back to your main page
-#   redirect '/meal-events'
-# end
-
-get('/recipes') do
-  erb(:recipes)
+get("/recipes") do
+  erb(:"shared/page")
 end
 
-get('/statistics') do
-  erb(:statistics)
+get("/statistics") do
+  erb(:"shared/page")
 end
-
-# class UsersController < ApplicationController
-#   def index
-#     firestore = initialize_firestore
-#     users_ref = firestore.col 'users'
-#     users = users_ref.get
-#     render({ :template => "users/index", :locals => { :users => users } })
-#   end
-# end
---
+++ public/recurrence.js
@@ -0,0 +1,181 @@
+window.onload = function () {
+  var modal = document.getElementById('meal-event-modal');
+  var createButton = document.getElementById('create-button');
+  createButton.addEventListener('click', function () {
+    modal.style.display = 'flex';
+  });
+
+  let cancelButton = document.getElementById('cancel-button');
+
+  cancelButton.addEventListener('click', function () {
+    modal.style.display = 'none';
+  });
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = 'none';
+    }
+  }
+
+  function createDropdownHTML(index) {
+    return `
+      <div class="dropdown" id="dropdown${index}" style="display: none;">
+        <button class="dropdown-button">Select:</button>
+        <div class="dropdown-content"></div>
+        <input type="hidden" id="recurrence${index}" name="recurrence${index}">
+      </div>
+    `;
+  }
+
+  var dropdownsContainer = document.getElementById('dropdowns-container');
+  var dropdownsCount = 4;
+  for (let i = 1; i <= dropdownsCount; i++) {
+    dropdownsContainer.innerHTML += createDropdownHTML(i);
+  }  
+
+  var dropdownDependencies = {
+    'START': ['Every', 'For'],
+    'Every': ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Day', 'Week', 'Month', 'Year'],
+    'For': ['1', '2', '3'],
+    'Monday': ['For', 'Until'],
+    'Tuesday': ['For', 'Until'],
+    'Wednesday': ['For', 'Until'],
+    'Thursday': ['For', 'Until'],
+    'Friday': ['For', 'Until'],
+    'Saturday': ['For', 'Until'],
+    'Sunday': ['For', 'Until'],
+    'Day': ['For', 'Until'],
+    'Week': ['For', 'Until'],
+    'Month': ['For', 'Until'],
+    'Year': ['For', 'Until'],
+    'For': ['1', '2', '3'],
+    'Until': ['Date'],
+    '1': [], // Add a corresponding array for '1'
+    '2': [], // Add a corresponding array for '2'
+    '3': [], // Add a corresponding array for '3'
+    'Date': [] // Add a corresponding array for 'Date'
+  };
+
+  // Initialize the dropdowns
+  var dropdowns = [];
+  for (let i = 1; i <= dropdownsCount; i++) {
+    dropdowns.push(createDropdown(`dropdown${i}`, ".dropdown-button", ".dropdown-content", `recurrence${i}`));
+  }
+
+  // Set the initial options for the first dropdown
+  updateDropdownOptions(dropdowns[0], dropdownDependencies['START']);
+
+  var isRecurrent = document.getElementById("is-recurrent");
+
+  function updateDropdownOptions(dropdown, options) {
+
+    dropdown.content.innerHTML = '';
+
+    // Add "Select:" as an option
+    if (options[0] !== "Select:") {
+      options.unshift("Select:");
+    }
+
+    for (let i = 0; i < options.length; i++) {
+      let p = document.createElement('p');
+      p.textContent = options[i];
+      p.setAttribute('data-value', options[i]);
+
+      p.onclick = function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        var previousValue = dropdown.input.value;
+        dropdown.button.textContent = p.textContent;
+        dropdown.input.value = p.getAttribute('data-value');
+        dropdown.content.style.display = 'none';
+        if (dropdown.input.value !== previousValue) {
+          var currentDropdown = dropdown.dropdown.nextElementSibling;
+          while (currentDropdown) {
+            var currentButton = currentDropdown.querySelector(".dropdown-button");
+            var currentInput = currentDropdown.querySelector('input[type="hidden"]');
+            currentDropdown.style.display = 'none';
+            currentButton.textContent = 'Select:';
+            currentInput.value = '';
+            currentDropdown = currentDropdown.nextElementSibling;
+          }
+        }
+        if (dropdown.dropdown.nextElementSibling) {
+          var nextDropdown = dropdown.dropdown.nextElementSibling;
+          var nextDropdownContent = nextDropdown.querySelector(".dropdown-content");
+          var nextOptions = dropdownDependencies[dropdown.input.value];
+          if (nextOptions) {
+            nextDropdown.style.display = 'inline-block';
+            updateDropdownOptions({
+              dropdown: nextDropdown,
+              button: nextDropdown.querySelector(".dropdown-button"),
+              content: nextDropdownContent,
+              input: nextDropdown.querySelector('input[type="hidden"]')
+            }, nextOptions);
+          }
+        }
+      };
+
+      dropdown.content.appendChild(p);
+    }
+
+    if (dropdown.dropdown.nextElementSibling) {
+      var currentDropdown = dropdown.dropdown.nextElementSibling;
+      while (currentDropdown) {
+        var currentButton = currentDropdown.querySelector(".dropdown-button");
+        var currentInput = currentDropdown.querySelector('input[type="hidden"]');
+        currentDropdown.style.display = 'none';
+        currentButton.textContent = 'Select:';
+        currentInput.value = '';
+        currentDropdown = currentDropdown.nextElementSibling;
+      }
+    }
+  }
+
+  function createDropdown(dropdownId, buttonId, contentId, inputId, nextDropdownId) {
+    var dropdown = document.getElementById(dropdownId);
+    var button = dropdown.querySelector(buttonId);
+    var content = dropdown.querySelector(contentId);
+    var input = document.getElementById(inputId);
+
+    button.onclick = function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+      content.style.display = content.style.display === 'block' ? 'none' : 'block';
+    };
+
+    // add this block of code
+    var initialOptions = Array.from(content.querySelectorAll('p')).map(function (p) {
+      return p.getAttribute('data-value');
+    });
+
+    updateDropdownOptions({
+      dropdown: dropdown,
+      button: button,
+      content: content,
+      input: input
+    }, initialOptions);
+
+    return {
+      dropdown: dropdown,
+      button: button,
+      content: content,
+      input: input
+    };
+  }
+
+  // If checkbox is unchecked, hide all dropdowns and reset their selections
+  isRecurrent.onchange = function () {
+    if (this.checked) {
+      dropdowns[0].dropdown.style.display = "inline-block";
+    } else {
+      dropdowns.forEach(function (dropdown) {
+        dropdown.dropdown.style.display = "none";
+        dropdown.button.textContent = "Select:";
+        dropdown.input.value = "";
+      });
+    }
+  }
+    
+  // Call the onchange function to set initial visibility of dropdowns
+  isRecurrent.onchange();
+}
