76348cf413d938b19c729f6df39c7552275342a4
Cisco Vlahakis <ciscovlahakis@gmail.com>
2023-11-18 02:24:13 +0000

    commit

--- app.rb
+++ app.rb
@@ -150,14 +150,12 @@ helpers do
   end
 
   def current_user
-    puts "Current session: #{session}"
     return unless session.fetch("user_id", nil)
     user_id = session.fetch("user_id", nil)
     return unless user_id
     users_ref = $firestore.col("users")
     user = users_ref.doc(user_id).get
     if user.exists?
-      puts "Found user: #{user.data}"
       @current_user = user.data.dup # Create a duplicate of user.data that is not frozen
       @current_user[:id] = user.document_id
       @current_user[:profile_picture] = user.data[:profile_picture]
