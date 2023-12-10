821bc53aaaa2c3aa2247223181e77a9d84124767
vlahakis <ciscovlahakis@gmail.com>
2023-12-10 12:28:45 -0600

    rehauling architecture

--- helpers/rendering_helpers.rb
+++ helpers/rendering_helpers.rb
@@ -4,16 +4,7 @@ require 'http'
 require 'json'
 require 'yaml'
  
-def fetch_page_data(route)
-  matching_pages = $db.col("pages").where("route", "=", route).get
-  first_page = matching_pages.first
-  if first_page.nil?
-    puts "PAGE NOT FOUND FOR: #{route}"
-    return nil
-  else
-    return first_page.data
-  end
-end
+
 
 def get_data(id, col)
   return nil if id.nil? || id.empty?
@@ -21,97 +12,4 @@ def get_data(id, col)
   _doc = _col.doc(id).get
   _data = _doc.data if _doc.exists?
   return _data
-end
-
-def build_components_structure(component_data)
-  return unless component_data.is_a?(Hash)
-  
-  if component_data.key?(:components)
-    return component_data.fetch(:components)
-  elsif component_data.key?(:componentId)
-    nested_component_data = get_data(component_data.fetch(:componentId), "components")
-    return unless nested_component_data
-    
-    if component_data.key?(:_yield)
-      yield_content = component_data.fetch(:_yield, nil)
-      replace_yield(nested_component_data, yield_content) if yield_content
-    end
-    
-    build_components_structure(nested_component_data)
-  end
-end
-
-def replace_yield(structure, yield_content)
-  structure.fetch(:components, []).map do |row|
-    next unless row && row.is_a?(Hash) && row.fetch(:row, nil)
-    if row[:row].is_a?(Array)
-      row[:row].map! { |value| value == "_yield" ? yield_content : value }
-    else
-      row[:row] = row[:row] == "_yield" ? yield_content : row[:row]
-    end
-  end
-end
-
-def parse_template_with_front_matter(template_content)
-  if template_content.start_with?("---")
-    front_matter_end = template_content.index("---", 3)
-    front_matter_yaml = template_content[3...front_matter_end].strip
-    front_matter_data = YAML.load(front_matter_yaml)
-    template_content = template_content[(front_matter_end+3)..-1]
-  else
-    front_matter_data = {}
-  end
-  
-  return template_content, front_matter_data
-end
-
-def collect_component_properties(structure)
-  structure.each do |component_struct|
-    components = component_struct.fetch(:row)
-    components = [components] unless components.is_a?(Array)
-
-    components.each do |component|
-      _, component_properties = fetch_component_properties(component)
-      @all_component_properties.merge!(component_properties) { |key, oldval, newval| [oldval, newval].flatten.uniq }
-    end
-  end
-end
-
-def fetch_component_properties(component)
-  response = HTTP.get("https://storage.googleapis.com/cisco-vlahakis.appspot.com/#{component}.erb")
-  template_content, front_matter_data = parse_template_with_front_matter(response.to_s)
-  return template_content, { component => front_matter_data }
-end
-
-def render_html_from_structure(structure)
-  html_content = "<div class='container'>"
-
-  structure.each do |component_struct|
-    components = component_struct.fetch(:row)
-
-    if components.is_a?(Array)
-      html_content += "<div class='row'>"
-      components.each do |component|
-        component_html = render_component_html(component)
-        html_content += component_html
-      end
-      html_content += "</div>" # Close the row div
-    else
-      component_html = render_component_html(components)
-      html_content += component_html
-    end
-  end
-
-  html_content += "</div>" # Close the container div
-  return html_content
-end
-
-def render_component_html(component)
-  response = HTTP.get("https://storage.googleapis.com/cisco-vlahakis.appspot.com/#{component}.erb")
-  template_content, _ = parse_template_with_front_matter(response.to_s)
-
-  page_data = @page_data
-  all_component_properties = @all_component_properties
-  erb_result = ERB.new(template_content).result(binding)
-  return erb_result
 end
--- routes/rendering_routes.rb
+++ routes/rendering_routes.rb
@@ -12,27 +12,89 @@ end
 get "/__sinatra__500.png" do
 end
 
-get "/*" do
-  route = request.path_info
-  @page_data = fetch_page_data(route)
+def fetch_page_data(route)
+  matching_pages = $db.col("pages").where("route", "=", route).get
+  first_page = matching_pages.first
+  if first_page.nil?
+    puts "PAGE NOT FOUND FOR: #{route}"
+    return nil
+  else
+    return first_page.data
+  end
+end
 
-  @all_component_properties = {}
-  html_content = "<p>PAGE NOT FOUND</p>"
+# Fetch the template from Google Cloud Storage
+def fetch_template(component_name)
+  response = HTTP.get("https://storage.googleapis.com/cisco-vlahakis.appspot.com/#{component_name}.erb")
+  if response.status.success?
+    return response.to_s
+  else
+    error_message = "Failed to fetch template: #{response.status}"
+    raise error_message
+  end
+end
 
-  if @page_data
-    component_structure = build_components_structure(@page_data)
+# Parse the YAML front matter from the template
+def parse_yaml_front_matter(template_content)
+  # Check if the file starts with the YAML front matter delimiter '---'
+  if template_content.strip.start_with?("---")
+    # Split on the YAML delimiters, ignoring the first split which is empty
+    front_matter, content = template_content.split(/---\s*/, 3)[1..]
+    parsed_front_matter = YAML.load(front_matter)
+    [parsed_front_matter, content.strip]
+  else
+    # If there is no YAML front matter, return an empty front matter and the whole content
+    [{}, template_content.strip]
+  end
+end
 
-    # Collect all properties before rendering components
-    collect_component_properties(component_structure)
-    
-    # Now render the components with the complete all_component_properties
-    html_content = render_html_from_structure(component_structure)
+get "/*" do |path|
+  # Fetch the page data from Firestore
+  page_data = fetch_page_data("/#{path}")
+
+  # Extract the template name from the page data
+  template_name = page_data.fetch(:template, nil)
+  if template_name.nil?
+    puts "Template name not specified in page data for path: #{path}"
+    halt 404, "Template not specified"
   end
 
-  # Render the layout template with the full all_component_properties
-  erb :layout, locals: {
-    :content => html_content,
-    :page_data => @page_data,
-    :all_component_properties => @all_component_properties
-  }
-end
+  # Fetch the template content from GCS
+  template_content = fetch_template(template_name)
+
+  # Extract YAML front matter and the HTML content
+  front_matter, html_content = parse_yaml_front_matter(template_content)
+
+  # Render each component and define a method in the current context
+  components = front_matter["components"] || []
+  components.each do |component|
+    # Fetch and render component
+    component_template_content = fetch_template(component)
+    _, component_html_content = parse_yaml_front_matter(component_template_content)
+    rendered_content = ERB.new(component_html_content).result(binding)
+
+    # Define a method within the current context
+    define_singleton_method(component) do
+      rendered_content
+    end
+  end
+ 
+  # Fetch and render the _yield component, if it exists
+  if page_data[:_yield]
+    yield_component_name = page_data[:_yield]
+    yield_component_template = fetch_template(yield_component_name)
+    _, yield_component_html_content = parse_yaml_front_matter(yield_component_template)
+    _yield_content = ERB.new(yield_component_html_content).result(binding)
+
+    # Define a method for the yield content
+    define_singleton_method('_yield') do
+      _yield_content
+    end
+  end
+ 
+   # Render the main template content using the current context
+   @html_content = ERB.new(html_content).result(binding)
+ 
+   # Render the final HTML content with the layout
+   erb :layout
+ end
