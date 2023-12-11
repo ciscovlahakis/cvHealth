0e2dace4d3705bb3352a0b18066a17a744355e63
vlahakis <ciscovlahakis@gmail.com>
2023-12-10 22:41:14 -0600

    fixing modal_box

--- routes/rendering_routes.rb
+++ routes/rendering_routes.rb
@@ -29,34 +29,30 @@ get "/*" do |path|
   # Extract YAML front matter and the HTML content
   front_matter, html_content = parse_yaml_front_matter(template_content)
 
-  # Initialize the resource_properties hash
-  resource_properties = {}
-
-  # Initialize the fragments_data hash
+  # Initialize the component_properties and fragments_data hashes
+  component_properties = {}
   fragments_data = {}
 
-  # Prepare the components array, including _yield if it exists
+  # Prepare the components array
   components = front_matter.fetch("components", [])
   _yield_component_name = page_data.fetch(:_yield, nil)
   components << _yield_component_name unless _yield_component_name.nil?
 
-  # Loop to collect properties for all components, including _yield
-  components.each do |component|
-    # Fetch component template content
-    component_template_content = fetch_template(component)
+  # First loop to collect properties for all components
+  components.each do |component_name|
+    component_template_content = fetch_template(component_name)
     next unless component_template_content
-    component_front_matter, component_html_content = parse_yaml_front_matter(component_template_content)
+    component_front_matter, _ = parse_yaml_front_matter(component_template_content)
 
-    # Store the component's properties in the resource_properties hash
-    resource_properties.store(component, component_front_matter)
+    # Store the component's properties in the component_properties hash
+    component_properties.store(component_name, component_front_matter)
 
+    # Process any fragments associated with the component
     fragment_file_names = component_front_matter.fetch("fragments", [])
-    # Fetch the content of each fragment
     fragment_file_names.each do |fragment_file_name|
       fragment_content = fetch_template(fragment_file_name)
       next unless fragment_content
       fragment_front_matter, fragment_html_content = parse_yaml_front_matter(fragment_content)
-      # Store the fragment's content in the fragments_data hash
       fragments_data.store(fragment_file_name, {
         :title => fragment_front_matter["title"],
         :content => fragment_html_content
@@ -64,45 +60,38 @@ get "/*" do |path|
     end
   end
 
-  # Render components with the full resource_properties context
-  components.each do |component|
-    # Fetch component template content again
-    _, component_html_content = parse_yaml_front_matter(fetch_template(component))
-
-    # Render the component with access to the resource_properties hash
-    rendered_content = ERB.new(component_html_content).result(binding)
-
-    # Define a method within the current context
-    define_singleton_method(component) do
-      rendered_content
-    end
-  end
-
-  # Special handling for _yield to replace it with the specified component content
-  if _yield_component_name
-    define_singleton_method('_yield') do
-      send(_yield_component_name)
-    end
-  end
-
-  # Parse the main HTML content and look for placeholders
+  # Parse the main HTML content with Nokogiri
   doc = Nokogiri::HTML(html_content)
-  doc.css('div[data-component]').each do |placeholder|
-    component_name = placeholder['data-component']
-    
-    # Check if the component method is defined
-    if respond_to?(component_name)
-      # Replace the placeholder div with the rendered component content
-      placeholder.replace(Nokogiri::HTML::DocumentFragment.parse(send(component_name)))
-    else
-      puts "Component method not defined for: #{component_name}"
-    end
+
+  # Second loop to render components and replace placeholders
+  components.each do |component_name|
+    component_template_content = fetch_template(component_name)
+    next unless component_template_content
+    _, component_html_content = parse_yaml_front_matter(component_template_content)
+
+    placeholder = doc.at("div[data-component='#{component_name}']")
+    placeholder = doc.at("div[data-component='_yield']") if component_name == _yield_component_name
+    next unless placeholder
+
+    rendered_content = if placeholder.inner_html.strip.empty?
+                         # No nested content, render the component as childless
+                         ERB.new(component_html_content).result(binding)
+                       else
+                         # Nested content found, treat the component as a parent
+                         component_doc = Nokogiri::HTML(component_html_content)
+                         yieldable_area = component_doc.at('div[data-yield]') # Adjust this selector based on your HTML structure
+                         yieldable_area.replace(placeholder.inner_html)
+                         component_doc.to_html
+                       end
+
+    # Replace the placeholder in the main document with the rendered content
+    placeholder.replace(Nokogiri::HTML::DocumentFragment.parse(rendered_content))
   end
 
   # After replacing all placeholders, convert the Nokogiri document back to HTML
   html_content_with_components = doc.to_html
 
-  # Render the main template content using the current context
+  # Final rendering of the page
   @html_content = ERB.new(html_content_with_components).result(binding)
 
   # Render the final HTML content with the layout
