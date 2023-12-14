bc37e50119d8f0351ee9d3e21fbd6adca2d6a14e
vlahakis <ciscovlahakis@gmail.com>
2023-12-13 20:26:16 -0600

    working on utility_panel

--- public/gcs/utility_panel.js
+++ public/gcs/utility_panel.js
@@ -1,6 +1,7 @@
 function initializeUtilityPanel() {
   var utilityPanel = document.querySelector('.utility-panel');
   var utilityContent = utilityPanel.querySelector('.utility-content');
+  var lastState = 'original'; // This will track the last non-minimized state
 
   // Helper function to toggle display styles
   function toggleDisplay(element, displayStyle) {
@@ -9,62 +10,64 @@ function initializeUtilityPanel() {
 
   document.body.addEventListener('click', function(event) {
     var content = utilityPanel.querySelector('[data-yield]');
-  
+
     if (event.target.id === 'close') {
       toggleDisplay(utilityPanel, 'none');
     } else if (event.target.id === 'expand') {
       var isExpanded = utilityPanel.classList.contains('modal');
       if (!isExpanded) {
         // Expanding to modal
-        utilityPanel.classList.remove('utility-panel');
         utilityPanel.classList.add('modal');
         utilityContent.classList.add('modal-content');
-        toggleDisplay(utilityPanel, 'flex');
-        utilityPanel.style.height = ''; // Clear any inline height style
-        toggleDisplay(content, 'block'); // Ensure content is visible
+        utilityPanel.classList.remove('utility-panel');
+        lastState = 'expanded'; // Remember the expanded state
       } else {
-        // Collapsing from modal to utility panel
+        // Collapsing to utility panel
         utilityPanel.classList.add('utility-panel');
-        utilityPanel.classList.remove('modal');
         utilityContent.classList.remove('modal-content');
-        toggleDisplay(utilityPanel, 'block');
-        utilityPanel.style.height = ''; // Clear any inline height style
+        utilityPanel.classList.remove('modal');
+        lastState = 'original'; // Revert back to the original state
       }
+      toggleDisplay(utilityPanel, 'block');
+      toggleDisplay(content, 'block'); // Ensure content is visible
+      utilityPanel.style.height = ''; // Clear any inline height style
     } else if (event.target.id === 'minimize') {
-      var isContentHidden = content.style.display === 'none';
-      toggleDisplay(content, isContentHidden ? 'block' : 'none');
-      utilityPanel.style.height = isContentHidden ? '400px' : 'auto';
-      
-      // If the utility panel is in the modal state, revert it back to the utility panel state
-      if (utilityPanel.classList.contains('modal')) {
-        utilityPanel.classList.add('utility-panel');
-        utilityPanel.classList.remove('modal');
-        utilityContent.classList.remove('modal-content');
+      var isContentVisible = content.style.display !== 'none';
+      if (isContentVisible) {
+        // Minimize the content
+        toggleDisplay(content, 'none');
+        utilityPanel.style.height = 'auto';
+      } else {
+        // Maximize the content to the last state
+        toggleDisplay(content, 'block');
+        utilityPanel.style.height = lastState === 'expanded' ? '' : '400px';
+        if (lastState === 'expanded') {
+          utilityPanel.classList.add('modal');
+          utilityContent.classList.add('modal-content');
+          utilityPanel.classList.remove('utility-panel');
+        }
       }
-      toggleDisplay(utilityPanel, 'block'); // This might be redundant, but ensures visibility
     }
   });
 
   // Add an event listener specifically to the utility content to stop propagation
-  if (utilityContent) {
-    utilityContent.addEventListener('click', function(event) {
-      event.stopPropagation(); // Prevent clicks inside the content from closing it
-    });
-  }
+  utilityContent.addEventListener('click', function(event) {
+    event.stopPropagation(); // Prevent clicks inside the content from closing it
+  });
 
+  // Event listener for clicks outside the modal to minimize it
   document.addEventListener('click', function(event) {
-    // Check if the utility panel is in a modal state
-    if (utilityPanel.classList.contains('modal')) {
-      // Check if the click is directly on the modal backdrop (`.modal`), not its children
-      if (event.target.classList.contains('modal')) {
-        // Minimize the utility panel
-        var content = utilityPanel.querySelector('[data-yield]');
-        toggleDisplay(content, 'none'); // Hide the content
-        utilityPanel.style.height = 'auto'; // Reset the height for the minimized state
-        utilityPanel.classList.remove('modal');
-        utilityContent.classList.remove('modal-content');
-        utilityPanel.classList.add('utility-panel'); // Add back the utility-panel class
-      }
+    var isModal = utilityPanel.classList.contains('modal');
+    var isClickInsideUtilityContent = utilityContent.contains(event.target);
+
+    if (isModal && !isClickInsideUtilityContent && event.target.id !== 'expand') {
+      // Minimize the utility panel
+      toggleDisplay(utilityContent, 'none');
+      utilityPanel.classList.add('utility-panel');
+      utilityContent.classList.remove('modal-content');
+      utilityPanel.classList.remove('modal');
+      lastState = 'original'; // Update the last non-minimized state
+      utilityPanel.style.height = 'auto';
     }
   }, true);
 }
