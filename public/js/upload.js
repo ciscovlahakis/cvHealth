0cf95085a0a84ffe1728765e4b8afe66ed8476ba
vlahakis <ciscovlahakis@gmail.com>
2023-12-15 19:17:29 -0600

    moved rendering of hashes to layout

---
+++ public/js/upload.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var form = document.getElementById('upload-form');
+  
+  form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+    
+    var formData = new FormData(form);
+    var currentHash = window.location.hash; // Capture the current hash
+    
+    fetch(form.action, {
+      method: 'POST',
+      body: formData
+    }).then(function(response) {
+      // Check the response status
+      if (!response.ok) {
+        throw new Error('Network response was not OK');
+      }
+      // Perform the redirect using the hash
+      window.location.hash = currentHash;
+      window.location.reload(true); // Force reload without cache
+    }).catch(function(error) {
+      console.error('There has been a problem with your fetch operation:', error);
+    });
+  });
+});
