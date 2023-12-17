e139f519ad7db57089a6a021b8c6ef003df63e28
vlahakis <ciscovlahakis@gmail.com>
2023-12-17 11:57:48 -0600

    fixed erb rendering issue

--- routes/rendering_routes.rb
+++ routes/rendering_routes.rb
@@ -40,8 +40,9 @@ get "/*" do |path|
     :value => collection_name
   }) if collection_name
 
-  # Extract YAML front matter and the HTML content
-  front_matter, html_content = parse_yaml_front_matter(template_content)
+  # Render ERB and extract YAML front matter and the HTML content
+  rendered_template_content = ERB.new(template_content).result(binding)
+  front_matter, html_content = parse_yaml_front_matter(rendered_template_content)
 
   @component_scripts = []
   component_properties = {}
@@ -56,7 +57,10 @@ get "/*" do |path|
   components.each do |component_name|
     component_template_content = fetch_template(component_name)
     next unless component_template_content
-    component_front_matter, _ = parse_yaml_front_matter(component_template_content)
+
+    # Render ERB and parse YAML front matter
+    rendered_component_content = ERB.new(component_template_content).result(binding)
+    component_front_matter, _ = parse_yaml_front_matter(rendered_component_content)
 
     # Store the component's properties in the component_properties hash
     component_properties.store(component_name, component_front_matter)
@@ -66,26 +70,32 @@ get "/*" do |path|
     fragment_file_names.each do |fragment_file_name|
       fragment_content = fetch_template(fragment_file_name)
       next unless fragment_content
-      fragment_front_matter, fragment_html_content = parse_yaml_front_matter(fragment_content)
+
+      # Render ERB and parse YAML front matter
+      rendered_fragment_content = ERB.new(fragment_content).result(binding)
+      fragment_front_matter, fragment_html_content = parse_yaml_front_matter(rendered_fragment_content)
 
       # Process nested components within the fragment
       nested_components = fragment_front_matter.fetch("components", [])
       nested_components.each do |nested_component_name|
         nested_component_template_content = fetch_template(nested_component_name)
         next unless nested_component_template_content
-        _, nested_component_html_content = parse_yaml_front_matter(nested_component_template_content)
+        
+        # Render ERB and parse YAML front matter
+        rendered_nested_component_content = ERB.new(nested_component_template_content).result(binding)
+        _, nested_component_html_content = parse_yaml_front_matter(rendered_nested_component_content)
 
         # Prepare the Nokogiri document for the nested component
         nested_doc = Nokogiri::HTML::DocumentFragment.parse(nested_component_html_content)
         nested_placeholder = nested_doc.at("div[data-yield]")
-      
+
         # Check for nested content within the parent component's placeholder
         fragment_doc = Nokogiri::HTML::DocumentFragment.parse(fragment_html_content)
         parent_placeholder = fragment_doc.at("div[data-component='#{nested_component_name}']")
-      
+
         # If nested content exists, extract it
         child_content = parent_placeholder.inner_html if parent_placeholder && !parent_placeholder.inner_html.strip.empty?
-      
+
         # Render the nested component with any child content
         rendered_nested_component = if nested_placeholder && child_content
                                       # Replace the placeholder in the nested component with the child content
@@ -93,18 +103,18 @@ get "/*" do |path|
                                       nested_doc.to_html
                                     else
                                       # No nested content; render the component as it is
-                                      ERB.new(nested_component_html_content).result(binding)
+                                      nested_component_html_content
                                     end
-      
+
         # Replace the placeholder for the nested component in the parent's content
         parent_placeholder.replace(rendered_nested_component) if parent_placeholder
-      
+
         # Update the fragment_html_content with the modified document
         fragment_html_content = fragment_doc.to_html
 
         @component_scripts << nested_component_name if File.exist?("./public/gcs/#{nested_component_name}.js")
       end
-      
+
       # Store the fully rendered fragment content
       fragments_data.store(fragment_file_name, {
         :title => fragment_front_matter["title"],
@@ -121,23 +131,16 @@ get "/*" do |path|
   components.each do |component_name|
     component_template_content = fetch_template(component_name)
     next unless component_template_content
-    _, component_html_content = parse_yaml_front_matter(component_template_content)
+
+    # Render ERB and parse YAML front matter
+    rendered_component_content = ERB.new(component_template_content).result(binding)
+    _, component_html_content = parse_yaml_front_matter(rendered_component_content)
 
     placeholder = doc.at("div[data-component='#{component_name}']")
     placeholder = doc.at("div[data-component='_yield']") if component_name == _yield_component_name
     next unless placeholder
 
-    rendered_content = if placeholder.inner_html.strip.empty?
-                         # No nested content, render the component as childless
-                         ERB.new(component_html_content).result(binding)
-                       else
-                         # Nested content found, treat the component as a parent
-                         component_doc = Nokogiri::HTML::DocumentFragment.parse(component_html_content)
-                         yieldable_area = component_doc.at('div[data-yield]')
-                         yieldable_area.inner_html = placeholder.inner_html
-                         component_doc.to_html
-                       end
-
+    rendered_content = ERB.new(component_html_content).result(binding)
     # Replace the placeholder in the main document with the rendered content
     placeholder.replace(Nokogiri::HTML::DocumentFragment.parse(rendered_content))
     @component_scripts << component_name if File.exist?("./public/gcs/#{component_name}.js")
@@ -151,7 +154,7 @@ get "/*" do |path|
 
   @component_scripts.uniq!
 
-  @fragments_data = fragments_data;
+  @fragments_data = fragments_data
 
   # Render the final HTML content with the layout
   erb :layout
