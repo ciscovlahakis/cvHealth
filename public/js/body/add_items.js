b71c136a558b1ffe286f230dfebf847fad108bfd
vlahakis <ciscovlahakis@gmail.com>
2023-12-20 13:39:13 -0600

    transfer fragment JS logic to components

--- public/js/body/add_items.js
+++ public/js/body/add_items.js
@@ -1,46 +1,5 @@
 
 function addItems(dataParentId, element) {
-  document.body.addEventListener('submit', function(event) {
-    if (event.target && event.target.id === 'add-items-form') {
-      event.preventDefault();
-  
-      var formData = new FormData(event.target);
-      var nestedHashData = {};
-      
-      for (var [key, value] of formData.entries()) {
-        nestedHashData[key] = { "value": value, "type": "string" }; // Default to string for simplicity
-        // You can add a condition to check if the value is numeric and set the type to "integer"
-        if (!isNaN(value) && value.trim() !== '') {
-          nestedHashData[key].type = "integer";
-          nestedHashData[key].value = parseInt(value, 10); // Convert to integer
-        }
-      }
-  
-      var actionUrl = event.target.action;
-      var jsonBody = JSON.stringify(nestedHashData);
-  
-      fetch(actionUrl, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: jsonBody
-      })
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-        // Handle the response, update the UI as needed
-        var currentUrlWithoutHash = window.location.href.split('#')[0];
-        window.history.pushState({}, '', currentUrlWithoutHash);
-        renderFragmentByHash();
-      })
-      .catch(function(error) {
-        console.error('Error:', error);
-      });
-    }
-  });
-
   PubSub.subscribe(dataParentId, function(data) {
     var dataId = element.dataset.id;
     var collection = data?.collection;
--- public/js/body/form.js
+++ public/js/body/form.js
@@ -1,13 +1,114 @@
-
 function form(dataParentId, element) {
-
-  // Set fields
-
-
+  // Subscribe to the dataParentId topic
   PubSub.subscribe(dataParentId, function(data) {
-    var collection = data?.collection;
-    var fields = data?.fields;
-    var action_url = data?.form_mode === "new" ? "/create/" : "/edit/";
-    action_url += collection;
+    console.log("hello")
+    // Destructure the data object
+    const { collection, fields, form_mode, item_data } = data;
+
+    // Determine the action URL based on the form mode
+    const action_url = form_mode === "new" ? "/create/" : "/edit/" + collection;
+    element.action = action_url; // Set form action URL
+
+    // Clear previous fields (if any)
+    while (element.firstChild) {
+      element.removeChild(element.firstChild);
+    }
+
+    // Assuming 'element' has the class 'form-container' as per the CSS
+    // If not, add the class to the element
+    element.classList.add('form-container');
+
+    // Create and insert form contents
+    const header = document.createElement('h2');
+    header.textContent = form_mode === "edit" ? "Edit Item" : "Add Item";
+    element.appendChild(header);
+
+    // Generate form fields
+    fields.forEach((field) => {
+      if (field.editable) {
+        const formGroup = document.createElement('div');
+        formGroup.className = 'form-group';
+
+        const label = document.createElement('label');
+        label.htmlFor = field.name;
+        label.textContent = field.title || '';
+        formGroup.appendChild(label);
+
+        const input = document.createElement('input');
+        input.type = 'text'; // Assuming the input type is text
+        input.id = field.name;
+        input.name = field.name;
+        input.value = item_data && item_data[field.name] ? item_data[field.name] : '';
+        input.required = form_mode === 'new';
+        formGroup.appendChild(input);
+
+        element.appendChild(formGroup);
+      }
+    });
+
+    // Create and insert form actions
+    const formActions = document.createElement('div');
+    formActions.className = 'form-actions';
+
+    const submitButton = document.createElement('input');
+    submitButton.type = 'submit';
+    submitButton.value = form_mode === "edit" ? "Update" : "Save";
+    formActions.appendChild(submitButton);
+
+    const cancelButton = document.createElement('button');
+    cancelButton.type = 'button';
+    cancelButton.textContent = 'Cancel';
+    cancelButton.onclick = function() { location.href = '/cancel-action'; };
+    formActions.appendChild(cancelButton);
+
+    element.appendChild(formActions);
+  });
+
+  document.body.addEventListener('submit', function(event) {
+    // Check if the submitted form is the one we are interested in
+    if (event.target && event.target === element) {
+      event.preventDefault();
+  
+      // Create a FormData object from the form
+      const formData = new FormData(element);
+      const nestedHashData = {};
+  
+      // Iterate over the FormData entries
+      for (const [key, value] of formData.entries()) {
+        // Determine the type of the value
+        const isNumericValue = !isNaN(value) && value.trim() !== '';
+        nestedHashData[key] = {
+          "value": isNumericValue ? parseInt(value, 10) : value,
+          "type": isNumericValue ? "integer" : "string"
+        };
+      }
+  
+      // Get the action URL from the form's 'action' attribute
+      const actionUrl = element.action;
+  
+      // Convert the nestedHashData to a JSON string
+      const jsonBody = JSON.stringify(nestedHashData);
+  
+      // Perform the fetch request with the JSON body
+      fetch(actionUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: jsonBody
+      })
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        // Handle the response, update the UI as needed
+        // Assuming there's a function to update the UI after form submission
+        // You will replace the below line with your function call
+        console.log('Form submitted successfully. Response:', data);
+      })
+      .catch(function(error) {
+        console.error('Error:', error);
+      });
+    }
   });
 }
