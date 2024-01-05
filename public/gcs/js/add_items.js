6f2d1aa2eb08db160b888c4bb6fe09b92c6ac4a9
vlahakis <ciscovlahakis@gmail.com>
2024-01-05 00:19:21 -0600

    fixing form

--- public/gcs/js/add_items.js
+++ public/gcs/js/add_items.js
@@ -4,10 +4,8 @@ function addItems(_, dataId, dataParentId) {
   ["collection", "fields", "onChildChanged"].forEach((x) => {
     on([dataParentId, x], () => {
       const props = getDoc(dataParentId);
-      const { collection, fields, onChildChanged, childId } = props;
-      if (childId !== dataId) {
-        onChildChanged({ childId: dataId });
-      }
+      const { collection, fields, onChildChanged } = props;
+      onChildChanged({ childId: dataId });
       upsertDoc(dataId, { collection, fields });
     }, dataId);
   });
--- public/gcs/js/table_controller.js
+++ public/gcs/js/table_controller.js
@@ -1,30 +1,31 @@
 
-function tableController(element, dataId, dataParentId) {
+function tableController(_, dataId, dataParentId) {
 
   on([dataParentId, "page"], async newValue => {
     const { collection } = newValue;
-    const fields = await fetchCollectionData(collection);
+    const fields = await fetchCollectionFields(collection);
     upsertDoc(dataId, { collection, fields });
   }, dataId);
 
   const onChildChanged = (childData) => {
-    childData ||= {};
-    const { columnIcon, rowClicked, childId } = childData;
-    upsertDoc(dataId, { columnIcon, rowClicked, childId });
+    const _childId = getDoc(dataId)?.childId;
+    const { columnIcon, rowClicked, childId } = childData || {};
+    if (!childId || _childId !== childId) {
+      upsertDoc(dataId, { columnIcon, rowClicked, childId });
+    }
   }
 
   upsertDoc(dataId, { onChildChanged });
 
-  // Fetch the collection's fields
-  async function fetchCollectionData(collectionName) {
+  async function fetchCollectionFields(collectionName) {
     if (!collectionName) return;
     try {
       var response = await fetch(`/api/collection/collections?field=name&value=${collectionName}`);
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
-      var collection_data = await response.json();
-      return collection_data.fields;
+      var collectionData = await response.json();
+      return collectionData.fields;
     } catch (error) {
       console.error('There has been a problem with your fetch operation:', error);
       return null;
